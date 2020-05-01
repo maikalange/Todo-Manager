4cb74dd51ded00b41f05143811e4aa924b943466
Joseph Nyirenda <jnj@sitecore.net>
2020-05-01 11:59:45 +0100

    Add project files.

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Education.TodoManager.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string searchTerm)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+
+namespace Sitecore.Education.TodoManager.Models
+{
+    public class Task
+    {
+        public DateTime DueDate { get; set; }
+        public HtmlString Description { get; set; }
+        public HtmlString Category { get; set; }
+        public HtmlString Status { get; set; }
+        public HtmlString Details { get; set; }
+    }
+}
