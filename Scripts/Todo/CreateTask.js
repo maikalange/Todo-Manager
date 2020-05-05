05431eb4822424f44f6b4f6e0359093bb366b588
Joseph Nyirenda <jnj@sitecore.net>
2020-05-05 18:26:50 +0100

    Added support for updating status and category on a task

--- Scripts/Todo/CreateTask.js
+++ Scripts/Todo/CreateTask.js
@@ -3,16 +3,18 @@
     ///sitecore/api/ssc/item/110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9
 
     var d = new Date(myForm.duedate.value).toISOString().replace(/-/g, '').replace(/:/g, '').split('.')[0];
-
+    var s = document.querySelector('#statusId option:checked').value;
+    var c = document.querySelector('#categoryId option:checked').value;
     var obj = {
         ItemName: myForm.description.value,
         TemplateID: '{3606EE62-7048-4136-89B4-D4BEAC3B5806}',
         Description: myForm.description.value,
         Details: myForm.details.value,
         DateDue: d,
-        Status: document.querySelector('#statusId option:checked').value,
-        Category: document.querySelector('#categoryId option:checked').value
+        Status: s,
+        Category:c
     };
+
     service.create(obj)
         .path(path)
         .execute()
@@ -22,4 +24,18 @@
         .fail(function (err) { window.alert(err); });
     event.preventDefault();
     return false;
+}
+
+function getItemById(id) {
+    var category = '';
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://rabbitsc.dev.local/sitecore/api/ssc/item/" + id);
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            category = (this.responseText);
+        }
+    };
+    xhr.send(null);
+
+    return category;
 }
