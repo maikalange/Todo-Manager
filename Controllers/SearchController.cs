e3fa0495112645e6e75a3aa02ffd064407aabc00
Joseph Nyirenda <jnj@sitecore.net>
2020-05-07 11:28:40 +0100

    Updated Search view

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,9 +1,7 @@
-﻿
-using Sitecore.ContentSearch;
+﻿using Sitecore.ContentSearch;
 using Sitecore.ContentSearch.Linq;
 using Sitecore.Education.TodoManager.Models;
 using Sitecore.Mvc.Presentation;
-using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -17,26 +15,22 @@ namespace Sitecore.Education.TodoManager.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Index(string searchTerm)
+        public ActionResult Index(string searchTerm, int pageNo = 1)
         {
             //Get the index
             //Create a search context and then perform the search
-    
-            return View();
+
+            return View(GetEventsList(searchTerm, pageNo));
         }
         private const int PageSize = 4;
-        public SearchResultsList GetEventsList(int pageNo)
+        public SearchResultsList GetEventsList(string searchTerm,int pageNo)
         {
-            var indexname =
-            $"todo_{RenderingContext.Current.ContextItem.Database.Name.ToLower()}_index";
-        var index = ContentSearchManager.GetIndex(indexname);
+            ViewBag.SearchTerm = searchTerm;
+            var indexname = $"todo_{RenderingContext.Current.ContextItem.Database.Name.ToLower()}_index";
+            var index = ContentSearchManager.GetIndex(indexname);
             using (var context = index.CreateSearchContext())
             {
                 var results = context.GetQueryable<TaskDetailsSearchItem>()
-                .Where(i =>
-                i.Paths.Contains(RenderingContext.Current.ContextItem.ID))
-                .Where(i => i.Language ==
-                RenderingContext.Current.ContextItem.Language.Name)
                 .Page(pageNo, PageSize)
                 .GetResults();
                 return new SearchResultsList()
