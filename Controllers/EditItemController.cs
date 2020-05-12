c39fa6270bfb74e7e034b155235939e98ef52c1c
Joseph Nyirenda <jnj@sitecore.net>
2020-05-12 14:21:08 +0100

    Added support for item editing

---
+++ Controllers/EditItemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Education.TodoManager.Controllers
+{
+    public class EditItemController : Controller
+    {
+        // GET: EditItem
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Scripts/Todo/EditItemTask.js
@@ -0,0 +1,16 @@
+﻿document.onload = function () {
+    getItem();
+}
+
+function getItem() {
+    var params = new URLSearchParams('Id');
+    var item = new ItemService({ url: "/sitecore/api/ssc/item" + params.get('Id') });
+    item.fetchItem(id).execute().then(function (task) {
+        task.should.be.an.instanceOf(ItemService.Item);
+        done();
+        console.log(task);
+        console.table(task)
+    }).fail(done);
+    console.table(item);
+    console.log(params);
+}
