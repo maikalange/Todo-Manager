5e354ed1bf128d7a725aefdbee578b9b460ad510
Joseph Nyirenda <jnj@sitecore.net>
2020-05-12 14:44:43 +0100

    Added EditItemTask.js

--- Scripts/Todo/EditItemTask.js
+++ Scripts/Todo/EditItemTask.js
@@ -4,8 +4,8 @@
 
 function getItem() {
     var params = new URLSearchParams('Id');
-    var item = new ItemService({ url: "/sitecore/api/ssc/item" + params.get('Id') });
-    item.fetchItem(id).execute().then(function (task) {
+    var item = new ItemService({ url: "/sitecore/api/ssc/item"});
+    item.fetchItem('{5B6AC191-817A-4570-B46C-F30CC5553045}').execute().then(function (task) {
         task.should.be.an.instanceOf(ItemService.Item);
         done();
         console.log(task);
