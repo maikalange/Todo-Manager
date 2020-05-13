8cf9dd65375cd5f927aeef59db740db22beae335
Joseph Nyirenda <jnj@sitecore.net>
2020-05-13 16:43:28 +0100

    Added delete task functionality

---
+++ Scripts/Todo/deleteTask.js
@@ -0,0 +1,18 @@
+﻿function deleteItem() {
+    var params = new URLSearchParams(window.location.search);
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("DELETE", `/sitecore/api/ssc/item/${params.get('Id')}?database=master`);
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            alert('Status: ' + this.status + '\nHeaders: ' + JSON.stringify(this.getAllResponseHeaders()) + '\nBody: ' + this.responseText);
+            if (this.status === 204) {
+                document.querySelector('#deleteStatus').textContent = " The item was successfully delete and will be removed from the site on the next publish";
+            } else {
+                document.querySelector('#deleteStatus').textContent = " The item has already been deleted by another user.";
+            }
+        }
+    };
+    xhr.send(null);
+}
+window.onload = deleteItem;
