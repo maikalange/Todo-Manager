88f899a15941a4963019123f4cedcdd73c9c5f5e
Joseph Nyirenda <jnj@sitecore.net>
2020-05-14 21:35:16 +0100

    Added Datatable to view tasks

---
+++ Scripts/Todo/alltasks.js
@@ -0,0 +1,24 @@
+﻿window.onload = function(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/sitecore/api/ssc/item/ED3BC3C1-43DB-4FE3-92CD-066401F1773A/children");
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            var items = JSON.parse(this.responseText);
+            var dataSet = [[items[0].Description], [items[1].Description], [items[2].Description], [items[3].Description]];
+            $(document).ready(function () {
+                $('#allTasks').DataTable({
+                    data: dataSet,
+                    columns: [
+                        { title: "Description" },
+                        //{ title: "Status" },
+                        //{ title: "Category" },
+                        //{ title: "Due Date" }
+                    ]
+                });
+            });
+        }
+    };
+    xhr.send(null);
+
+}
+
