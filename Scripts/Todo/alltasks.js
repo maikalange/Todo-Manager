80cef4616599056d6368c50c75a675f97510a41c
Joseph Nyirenda <jnj@sitecore.net>
2020-05-14 21:48:24 +0100

    Added date support for items in grid

--- Scripts/Todo/alltasks.js
+++ Scripts/Todo/alltasks.js
@@ -1,18 +1,18 @@
-﻿window.onload = function(){
+﻿window.onload = function () {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "/sitecore/api/ssc/item/ED3BC3C1-43DB-4FE3-92CD-066401F1773A/children");
     xhr.onreadystatechange = function () {
         if (this.readyState == 4) {
             var items = JSON.parse(this.responseText);
-            var dataSet = [[items[0].Description], [items[1].Description], [items[2].Description], [items[3].Description]];
+            var dataSet = [[items[0].Description, items[0].Status, items[0].Category, moment.parseZone(items[0].DateDue,'YYYY-MM-DD')]];
             $(document).ready(function () {
                 $('#allTasks').DataTable({
                     data: dataSet,
                     columns: [
                         { title: "Description" },
-                        //{ title: "Status" },
-                        //{ title: "Category" },
-                        //{ title: "Due Date" }
+                        { title: "Status" },
+                        { title: "Category" },
+                        { title: "Due Date" }
                     ]
                 });
             });
