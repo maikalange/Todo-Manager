efc70432b4b7fcdf76a3dc17b17b9861cfa01695
Joseph Nyirenda <jnj@sitecore.net>
2020-05-15 10:43:54 +0100

    Fixed issues with data table

--- Scripts/Todo/alltasks.js
+++ Scripts/Todo/alltasks.js
@@ -1,10 +1,16 @@
 ﻿window.onload = function () {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/sitecore/api/ssc/item/ED3BC3C1-43DB-4FE3-92CD-066401F1773A/children");
+    xhr.open("GET", "/sitecore/api/ssc/item/ED3BC3C1-43DB-4FE3-92CD-066401F1773A/children?database=web");
     xhr.onreadystatechange = function () {
         if (this.readyState == 4) {
             var items = JSON.parse(this.responseText);
-            var dataSet = [[items[0].Description, items[0].Status, items[0].Category, moment.parseZone(items[0].DateDue,'YYYY-MM-DD')]];
+            var dataSet = [];
+
+            for (var i = 0; i < items.length; i++) {
+                dataSet.push([items[i].Description, items[i].Status,
+                items[i].Category, moment(moment.parseZone(items[i].DateDue, 'YYYY-MM-DD'))]);
+            }
+      
             $(document).ready(function () {
                 $('#allTasks').DataTable({
                     data: dataSet,
