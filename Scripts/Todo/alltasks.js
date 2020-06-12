7f5adff08c70233db2251843876566bb6fc33f48
Joseph Nyirenda <jnj@sitecore.net>
2020-06-12 14:53:14 +0100

    Added new feature

--- Scripts/Todo/alltasks.js
+++ Scripts/Todo/alltasks.js
@@ -1,16 +1,11 @@
 ﻿window.onload = function () {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/sitecore/api/ssc/item/ED3BC3C1-43DB-4FE3-92CD-066401F1773A/children?database=web");
+    xhr.open("GET", "/sitecore/api/ssc/item/ED3BC3C1-43DB-4FE3-92CD-066401F1773A/children?database=master");
     xhr.onreadystatechange = function () {
         if (this.readyState == 4) {
             var items = JSON.parse(this.responseText);
             var dataSet = [];
-
-            for (var i = 0; i < items.length; i++) {
-                dataSet.push([items[i].Description, items[i].Status,
-                items[i].Category, moment(moment.parseZone(items[i].DateDue, 'YYYY-MM-DD'))]);
-            }
-      
+            items.forEach(task => dataSet.push([task.Description, task.Status, task.Category, moment.parseZone(task.DateDue, 'YYYY-MM-DD')]));
             $(document).ready(function () {
                 $('#allTasks').DataTable({
                     data: dataSet,
